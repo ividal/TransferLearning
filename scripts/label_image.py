576926176ab0ff044af43e770963e9f65c7c3445
I. Vidal <irina.vidal+github@gmail.com>
2018-11-12 11:46:18 +0100

    clean-up labelling script

--- scripts/label_image.py
+++ scripts/label_image.py
@@ -72,58 +72,40 @@ def load_labels(label_file):
 
 
 if __name__ == "__main__":
-    file_name = "tf_files/flower_photos/daisy/3475870145_685a19116d.jpg"
-    model_file = "tf_files/retrained_graph.pb"
-    label_file = "tf_files/retrained_labels.txt"
-    input_height = 224
-    input_width = 224
-    input_mean = 128
-    input_std = 128
-    input_layer = "input"
-    output_layer = "final_result"
+    """
+    The expected way and paths used to call this script is as follows:
+    python -m scripts.label_image \
+      --model_file="tf_files/retrained_graph_${WIDTH}.pb" \
+      --labels=tf_files/retrained_labels.txt \
+      --image="tf_files/flower_photos/daisy/3475870145_685a19116d.jpg"
+    """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--image", help="image to be processed")
-    parser.add_argument("--graph", help="graph/model to be executed")
-    parser.add_argument("--labels", help="name of file containing labels")
-    parser.add_argument("--input_height", type=int, help="input height")
-    parser.add_argument("--input_width", type=int, help="input width")
-    parser.add_argument("--input_mean", type=int, help="input mean")
-    parser.add_argument("--input_std", type=int, help="input std")
-    parser.add_argument("--input_layer", help="name of input layer")
-    parser.add_argument("--output_layer", help="name of output layer")
+    parser.add_argument("--image_path", type=str, help="image to be processed", required=False)
+    parser.add_argument("--model_file", type=str, help="graph/model to be executed", required=True)
+    parser.add_argument("--labels", type=str, help="name of file containing labels", required=False)
+    parser.add_argument("--input_height", type=int, help="input height", default=224)
+    parser.add_argument("--input_width", type=int, help="input width", default=224)
+    parser.add_argument("--input_mean", type=int, help="input mean", default=128)
+    parser.add_argument("--input_std", type=int, help="input std", default=128)
+    parser.add_argument("--input_layer", type=str, help="name of input layer", default="input")
+    parser.add_argument("--output_layer", type=str, help="name of output layer", default="final_result")
     args = parser.parse_args()
 
-    if args.graph:
-        model_file = args.graph
-    if args.image:
-        file_name = args.image
     if args.labels:
         label_file = args.labels
-    if args.input_height:
-        input_height = args.input_height
-    if args.input_width:
-        input_width = args.input_width
-    if args.input_mean:
-        input_mean = args.input_mean
-    if args.input_std:
-        input_std = args.input_std
-    if args.input_layer:
-        input_layer = args.input_layer
-    if args.output_layer:
-        output_layer = args.output_layer
 
-    graph = load_graph(model_file)
-    t = read_tensor_from_image_file(file_name,
-                                    input_height=input_height,
-                                    input_width=input_width,
-                                    input_mean=input_mean,
-                                    input_std=input_std)
+    graph = load_graph(args.model_file)
+    t = read_tensor_from_image_file(args.image_path,
+                                    input_height=args.input_height,
+                                    input_width=args.input_width,
+                                    input_mean=args.input_mean,
+                                    input_std=args.input_std)
 
-    input_name = "import/" + input_layer
-    output_name = "import/" + output_layer
-    input_operation = graph.get_operation_by_name(input_name);
-    output_operation = graph.get_operation_by_name(output_name);
+    input_name = "import/" + args.input_layer
+    output_name = "import/" + args.output_layer
+    input_operation = graph.get_operation_by_name(input_name)
+    output_operation = graph.get_operation_by_name(output_name)
 
     with tf.Session(graph=graph) as sess:
         start = time.time()
@@ -133,7 +115,7 @@ if __name__ == "__main__":
     results = np.squeeze(results)
 
     top_k = results.argsort()[-5:][::-1]
-    labels = load_labels(label_file)
+    labels = load_labels(args.labels)
 
     print('\nEvaluation time (1-image): {:.3f}s\n'.format(end - start))
 
