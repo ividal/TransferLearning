0de9e98a05dd4dba0b7aaff556f70eb57c5fc639
I. Vidal <irina.vidal+github@gmail.com>
2018-11-27 03:00:51 +0100

    Init: logs with steps

---
+++ sh/retrain.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# 224, 192, 160 or 128
+IMAGE_SIZE=128
+
+# 1.0, 0.75, 0.50 or 0.25
+WIDTH=1.0
+
+# adam, sgd or default
+OPTIMIZER="adam"
+LEARNING_RATE=0.001
+BATCH_SIZE=8
+STEPS=800
+ARCH="mobilenet"
+LABEL="$ARCH-bs_$BATCH_SIZE-lr_$LEARNING_RATE-opt_$OPTIMIZER"
+
+python -m scripts.retrain \
+  --model_dir=tf_files/models/retrained \
+  --summaries_dir=tf_files/training_summaries/$LABEL \
+  --output_labels=tf_files/models/retrained/retrained_labels.npy \
+  --image_dir=tf_files/split_flowers \
+  --how_many_training_steps=$STEPS \
+  --learning_rate=$LEARNING_RATE\
+  --train_batch_size=$BATCH_SIZE \
+  --optimizer_name=$OPTIMIZER \
+  --image_size=$IMAGE_SIZE
